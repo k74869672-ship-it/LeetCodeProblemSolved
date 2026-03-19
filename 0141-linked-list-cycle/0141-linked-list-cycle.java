517488a33314ebb076e4133406150453584578c7
kajalpate <k74869672@gmail.com>
2026-03-19 19:45:14 +0530

    Time: 0 ms (100%), Space: 46.6 MB (83.21%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,6 +13,9 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
+         if(head==null||head.next==null){
+            return false;
+         }
 
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
