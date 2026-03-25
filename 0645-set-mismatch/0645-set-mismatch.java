56bcfe9512f96f1301854f18aa1a120bfeec67c9
kajalpatel <k74869672@gmail.com>
2026-03-25 11:16:15 +0530

    Time: 757 ms (5.01%), Space: 47.4 MB (92.29%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int repeated = -1;
+        int missing = -1;
+
+        for (int i = 1; i <= nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    count++;
+
+                }
+            }
+            if (count == 2) {
+                repeated = i;
+            } else if (count == 0) {
+                missing = i;
+            }
+        }
+        return new int[] { repeated, missing };
+
+    }
+}
