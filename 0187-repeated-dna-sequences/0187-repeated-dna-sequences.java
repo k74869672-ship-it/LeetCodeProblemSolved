add0371eec7e400764953b1f9d95afbfc8a9a596
Kajal Patel <k74869672@gmail.com>
2026-06-21 13:14:18 +0530

    Time: 21 ms (50.11%), Space: 55.8 MB (25.08%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen=new HashSet<>();
+        HashSet<String> repeated=new HashSet<>();
+
+        for(int i=0;i<=s.length()-10;i++){
+            String currentWindows=s.substring(i,i+10);
+
+            if(seen.contains(currentWindows)){
+                repeated.add(currentWindows);
+            }
+            else{
+                seen.add(currentWindows);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }
+}
