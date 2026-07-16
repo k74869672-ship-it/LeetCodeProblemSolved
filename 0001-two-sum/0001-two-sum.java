82bcf1f9c30bd938a7990eae38fb204286996296
Kajal Patel <k74869672@gmail.com>
2026-07-16 18:41:00 +0530

    Time: 2 ms (99.3%), Space: 47.2 MB (40.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+        
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
+            
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
             }
+            
             map.put(nums[i], i);
         }
-        return new int[] {};
+        
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
